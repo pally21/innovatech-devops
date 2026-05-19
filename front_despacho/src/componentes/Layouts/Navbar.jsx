709d513205235584ea46db32b04cc7d7ef05d8c5
pally21 <joha.gajardo@duocuc.cl>
2026-05-19 00:16:43 -0400

    feat: cambiar color navbar de teal a azul

--- front_despacho/src/componentes/Layouts/Navbar.jsx
+++ front_despacho/src/componentes/Layouts/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="rounded-xl w-[250px] min-h-[880px] bg-teal-600 text-white sticky top-0 p-4 m-4">
+    <nav className="rounded-xl w-[250px] min-h-[880px] bg-blue-700 text-white sticky top-0 p-4 m-4">
       {/* Logo o título */}
       <h2 className="text-xl font-bold mb-8">Despacho Dashboard</h2>
 
@@ -9,7 +9,7 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            className="block font-bold py-2 px-3 hover:bg-blue-800 rounded"
           >
             Usuarios
           </a>
@@ -17,7 +17,7 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            className="block font-bold py-2 px-3 hover:bg-blue-800 rounded"
           >
             Productos
           </a>
@@ -25,7 +25,7 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            className="block font-bold py-2 px-3 hover:bg-blue-800 rounded"
           >
             Configuración
           </a>
